354e5dc5a20e0a0b8cda67e3f78abf73e76cb9d5
dfm555 <duber@doitwiser.com>
2015-03-09 21:29:51 -0500

    first commit

--- js/script.js
+++ js/script.js
@@ -4,5 +4,28 @@
 $(document).ready(function(){
     $('#colombia').on('mouseenter',function(){
         $('.colombia').removeClass('hidden')
-    })
+    });
+    $('#colombia').on('mouseleave',function(){
+        $('.colombia').addClass('hidden')
+    });
+    $('#australia').on('mouseenter',function(){
+        $('.australia').removeClass('hidden')
+    });
+    $('#australia').on('mouseleave',function(){
+        $('.australia').addClass('hidden')
+    });
+    $('#canada').on('mouseenter',function(){
+        $('.canada').removeClass('hidden')
+    });
+    $('#canada').on('mouseleave',function(){
+        $('.canada').addClass('hidden')
+    });
+    $('#espana').on('mouseenter',function(){
+        $('.espana').removeClass('hidden')
+    });
+    $('#espana').on('mouseleave',function(){
+        $('.espana').addClass('hidden')
+    });
 });
+
+
